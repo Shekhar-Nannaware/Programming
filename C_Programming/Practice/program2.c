1667cef1b223990008abbcbda3336c3b7336ff96
Shekhar Nannaware <nannawareshekhar092@gmail.com>
2026-07-06 23:19:08 +0530

    Move C practice programs to C_Programming/Practice

--- C_Programming/Practice/program2.c
+++ C_Programming/Practice/program2.c
@@ -1,8 +1,77 @@
+///////////////////////////////////////////////////////
+//
+//  Program :      Student Information System
+//
+///////////////////////////////////////////////////////
+
 #include<stdio.h>
+
+///////////////////////////////////////////////////////
+//
+//  Function Name : main
+//  Input :         Name, Age, Percentage, Grade
+//  Output :        Displays Student Information
+//  Description :   Accepts student details from user
+//                  and displays them on the screen.
+//  Author :        Shekhar Jalindar Nannaware
+//  Date :          05/07/2026
+//
+///////////////////////////////////////////////////////
 
 int main()
 {
-    printf("Jay Ganesh...\n");
+    char Name[50];
+    int Age;
+    float Percentage;
+    char Grade;
+
+    ///////////////////////////////////////////////////
+    // Accept Student Information
+    ///////////////////////////////////////////////////
+
+    printf("Enter Name : ");
+    scanf("%s", Name);
+
+    printf("Enter Age : ");
+    scanf("%d", &Age);
+
+    printf("Enter Percentage : ");
+    scanf("%f", &Percentage);
+
+    printf("Enter Grade : ");
+    scanf(" %c", &Grade);
+
+    ///////////////////////////////////////////////////
+    // Display Student Information
+    ///////////////////////////////////////////////////
+
+    printf("\n---------------------------------\n");    
+    printf("      Student Information\n");
+    printf("---------------------------------\n");
+
+    printf("Name        : %s\n", Name);
+    printf("Age         : %d\n", Age);
+    printf("Percentage  : %.1f\n", Percentage);
+    printf("Grade       : %c\n", Grade);
 
     return 0;
 }
+
+///////////////////////////////////////////////////////
+//
+//  Input :
+//      Name        : Shekhar
+//      Age         : 22
+//      Percentage  : 58.0
+//      Grade       : A
+//
+//  Output :
+//
+//      Student Information
+//
+//      Name        : Shekhar
+//      Age         : 22
+//      Percentage  : 58.0
+//      Grade       : A
+//
+///////////////////////////////////////////////////////
