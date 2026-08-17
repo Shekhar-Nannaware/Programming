499afc31c6c3dd210adc0e4a0409959dd94092cf
Shekhar Nannaware <nannawareshekhar092@gmail.com>
2026-08-17 16:38:19 +0530

    Added Java practice program 52 to 74

--- Java/Practice/program15.java
+++ Java/Practice/program15.java
@@ -33,4 +33,8 @@ public class program15
 
         sc.close();
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
--- Java/Practice/program18.java
+++ Java/Practice/program18.java
@@ -33,4 +33,8 @@ public class program18
 
         sc.close();
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
