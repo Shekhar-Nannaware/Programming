bc485ba50b338d8f3d6ec199057d80a6aa0d41b2
Shekhar Nannaware <nannawareshekhar092@gmail.com>
2026-08-20 15:02:32 +0530

    Add Java Competitive 11 to 21

---
+++ Java/Competitive/program019.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//   Class Name : program019
+//   Description: Reverses the Digits of an Integer
+//   Date       : 18/08/2026
+//   Author     : Shekhar Jalindar Nannaware
+//
+/////////////////////////////////////////////////////////////////
+
+public class program019
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int iNo = 0;                     // To Store Number
+        int Reverse = 0;                 // To Store Reverse Number
+        int iDigit = 0;                  // To Store Last Digit
+
+        System.out.print("Enter Number : ");
+        iNo = sc.nextInt();
+
+        while(iNo > 0)
+        {
+            iDigit = iNo % 10;
+            Reverse = Reverse * 10 + iDigit;
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Reverse Number is : " + Reverse);
+
+        sc.close();
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//   Application to reverse the Digits of an Integer
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//   Input  : 12345
+//   Output : Reverse Number is : 54321
+//
+/////////////////////////////////////////////////////////////////
---
+++ Java/Competitive/program021.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////
+//
+//   Class Name : program021
+//   Description: Finds the Largest Digit in an Integer
+//   Date       : 18/08/2026
+//   Author     : Shekhar Jalindar Nannaware
+//
+/////////////////////////////////////////////////////////////////
+
+class LargestDigit
+{
+    public void CheckLargest(int iNo)
+    {
+        int iDigit = 0;                  // To Store Last Digit
+        int Largest = 0;                 // To Store Largest Digit
+
+        while(iNo > 0)
+        {
+            iDigit = iNo % 10;
+
+            if(iDigit > Largest)
+            {
+                Largest = iDigit;
+            }
+
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Largest Digit is : " + Largest);
+    }
+}
+
+public class program021
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int iNo = 0;                      // To Store Number
+
+        System.out.print("Enter Number : ");
+        iNo = sc.nextInt();
+
+        LargestDigit obj = new LargestDigit();
+
+        obj.CheckLargest(iNo);
+
+        sc.close();
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//   Application to find the Largest Digit in an Integer
+//
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//   Input  : 58321
+//   Output : Largest Digit is : 8
+//
+/////////////////////////////////////////////////////////////////
